b2bb4126284c40b964db072c4a8769bedb3cd677
Dmitry Sidorov <demsys14@gmail.com>
2021-12-10 23:38:37 +0300

    Random 300 - 700

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'RxJSSubjects';
+  timerID: any;
+  result: any;
+
+  stream = new BehaviorSubject(0);
+
+  constructor() { };
+
+  ngOnInit() {
+
+    this.stream.pipe(filter((el:any) => (el >= 300) && (el <= 700))).subscribe((value: any) => this.result = value);
+    this.timerID = setInterval(() => {
+      const newValue = Math.floor(Math.random() * 1000);
+      this.stream.next(newValue)
+    }, 500);
+    
+  };
+
+  stop() {
+    clearInterval(this.timerID)
+  }
+
 }
